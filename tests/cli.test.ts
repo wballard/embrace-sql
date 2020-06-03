f5bbbc9366079e58aaa868cc6fd8d5c40f26d790
Will Ballard <wballard@mailframe.net>
2020-06-03 10:07:14 -0500

    fixes #27

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -1,5 +1,6 @@
 import init from "../src/cli/init";
 import embedded from "../src/cli/embedded";
+import migrate from "../src/cli/migrate";
 import sinon from "sinon";
 import path from "path";
 import readFile from "read-file-utf8";
@@ -21,11 +22,27 @@ describe("CLI", () => {
   });
   it("generates an embedded client", async () => {
     const root = path.resolve(path.join(__dirname, "..", ".tests", "embedded"));
-    const command = await embedded.parseAsync(["node", "embedded", root, "--nowatch"]);
+    const command = await embedded.parseAsync([
+      "node",
+      "embedded",
+      root,
+      "--nowatch",
+    ]);
     // trick needed to wait out
     await Promise.all(command.parent?._actionResults || []);
     expect(command.args).toMatchSnapshot();
     // check on the key files
+    expect(await readFile(path.join(root, "index.ts"))).toMatchSnapshot();
+  });
+  it("migrates", async () => {
+    const root = path.resolve(path.join(__dirname, "..", ".tests", "migrate"));
+    const spy = sandbox.spy(console, "info");
+    const command = await migrate.parseAsync(["node", "migrate", root]);
+    // trick needed to wait out
+    await Promise.all(command.parent?._actionResults || []);
+    const output = spy.getCall(0).args[0];
+    expect(output).toMatchSnapshot();
+    // and some files to exist
     expect(await readFile(path.join(root, "index.ts"))).toMatchSnapshot();
   });
 });
